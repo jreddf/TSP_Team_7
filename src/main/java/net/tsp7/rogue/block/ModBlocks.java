51ee07bf0eaf247b58772811e21ea4117ed302a9
sapatt <sapatter@mtu.edu>
2025-03-10 16:46:48 -0400

    uploaded custom dimension
    had to fix a datagen issue in build.grade but that is taken care of, for future reference if we ever change the dimension run ./gradlew runDatagen which will build the data needed to ensure that the dimension is actually recognized as a dimension we can use

---
+++ src/main/java/net/tsp7/rogue/block/ModBlocks.java
@@ -0,0 +1,36 @@
+package net.tsp7.rogue.block;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.tsp7.rogue.Rogue;
+
+public class ModBlocks {
+    // This is the code that creates the block, use this to create your own blocks.
+    // You can either copy settings from existing vanilla blocks, or modify them manually.
+    // If you copy and want to modify some values make sure the copy comes first
+    public static final Block EXAMPLE_BLOCK = registerBlock("example_block",
+            new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
+
+
+    // Everything below are helper methods for creating new blocks. Do not modify
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, new Identifier(Rogue.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block) {
+        return Registry.register(Registries.ITEM, new Identifier(Rogue.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
+    }
+
+    public static void registerModBlocks() {
+        Rogue.LOGGER.info("Registering ModBlocks for " + Rogue.MOD_ID);
+    }
+}
