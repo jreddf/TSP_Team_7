973b9df8ccbe5c413147c85437ac479f94e5af3c
sapatt <sapatter@mtu.edu>
2025-03-21 12:37:35 -0400

    Add files via upload

--- rogue/src/main/java/net/tsp7/rogue/Rogue.java
+++ rogue/src/main/java/net/tsp7/rogue/Rogue.java
@@ -4,9 +4,12 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
-import net.minecraft.block.Blocks;
-import net.minecraft.item.Items;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.tsp7.rogue.Enemy.roguebaseenemy;
 import net.tsp7.rogue.block.ModBlocks;
 import net.tsp7.rogue.item.ModItemGroups;
 import net.tsp7.rogue.item.ModItems;
@@ -18,16 +21,26 @@ public class Rogue implements ModInitializer {
 	public static final String MOD_ID = "rogue";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final EntityType<roguebaseenemy> Enemy = Registry.register(
+			Registries.ENTITY_TYPE,
+			Identifier.of("entitytesting", "rogue"),
+			EntityType.Builder.create(roguebaseenemy::new, SpawnGroup.CREATURE).setDimensions(0.75f, 0.75f).build("rogue")
+	);
+
+
 	@Override
 	public void onInitialize() {
 		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+
+		FabricDefaultAttributeRegistry.register(Enemy, roguebaseenemy.createMobAttributes());
+
 		CustomPortalBuilder.beginPortal()
-				.frameBlock(Blocks.REINFORCED_DEEPSLATE)
-				.lightWithItem(Items.NETHERITE_SWORD)
+				.frameBlock(ModBlocks.EXAMPLE_BLOCK)
+				.lightWithItem(ModItems.EXAMPLE_ITEM)
 				.destDimID(new Identifier(Rogue.MOD_ID, "roguedimension"))
-				.tintColor(50,200,255).
+				.tintColor(255,10,127).
 				registerPortal();
 	}
 }
--- rogue/src/main/java/net/tsp7/rogue/RogueClient.java
+++ rogue/src/main/java/net/tsp7/rogue/RogueClient.java
@@ -1,10 +1,20 @@
 package net.tsp7.rogue;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.render.entity.model.EntityModelLayer;
+import net.minecraft.util.Identifier;
+import net.tsp7.rogue.Enemy.rogueRenderer;
+import net.tsp7.rogue.Enemy.roguebaseenemyModel;
 
 public class RogueClient implements ClientModInitializer {
+    public static final EntityModelLayer MODEL_CUBE_LAYER = new EntityModelLayer(new Identifier("entitytesting", "rogue"), "main");
     @Override
     public void onInitializeClient() {
+        EntityRendererRegistry.register(Rogue.Enemy, rogueRenderer::new);
 
+
+        EntityModelLayerRegistry.registerModelLayer(MODEL_CUBE_LAYER, roguebaseenemyModel::getTexturedModelData);
     }
 }
