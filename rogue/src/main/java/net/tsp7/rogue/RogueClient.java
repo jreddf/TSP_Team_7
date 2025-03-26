343ad642bb07baa0fa515700b6aecf6263ce336b
sapatt <sapatter@mtu.edu>
2025-03-26 12:23:30 -0400

    Update RogueClient.java

--- rogue/src/main/java/net/tsp7/rogue/RogueClient.java
+++ rogue/src/main/java/net/tsp7/rogue/RogueClient.java
@@ -10,9 +10,13 @@ import net.tsp7.rogue.entity.client.EvilGolemRenderer;
 import net.tsp7.rogue.entity.client.ModModelLayers;
 
 public class RogueClient implements ClientModInitializer {
+    public static final EntityModelLayer MODEL_CUBE_LAYER = new EntityModelLayer(new Identifier("entitytesting", "rogue"), "main");
     @Override
     public void onInitializeClient() {
         EntityRendererRegistry.register(ModEntities.EVIL_GOLEM, EvilGolemRenderer::new);
         EntityModelLayerRegistry.registerModelLayer(ModModelLayers.EVIL_GOLEM, EvilGolemModel::getTexturedModelData);
+
+        EntityRendererRegistry.register(Rogue.Enemy, rogueRenderer::new);
+        EntityModelLayerRegistry.registerModelLayer(MODEL_CUBE_LAYER, roguebaseenemyModel::getTexturedModelData);
     }
 }
