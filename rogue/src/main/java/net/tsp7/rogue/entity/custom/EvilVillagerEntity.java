ff31224d04154a7c0e26a142381ca1ef3c08ec3b
sapatt <sapatter@mtu.edu>
2025-04-07 17:05:45 -0400

    Add files via upload

---
+++ rogue/src/main/java/net/tsp7/rogue/entity/custom/EvilVillagerEntity.java
@@ -0,0 +1,160 @@
+package net.tsp7.rogue.entity.custom;
+
+import net.minecraft.entity.AnimationState;
+import net.minecraft.entity.EntityPose;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.world.World;
+import net.tsp7.rogue.entity.ai.EvilGolemAttackGoal;
+import net.tsp7.rogue.entity.ai.EvilVillagerAttackGoal;
+import net.tsp7.rogue.entity.custom.EvilGolemEntity;
+import org.jetbrains.annotations.Nullable;
+
+public class EvilVillagerEntity extends HostileEntity {
+    private static final TrackedData<Boolean> ATTACKING =
+            DataTracker.registerData(EvilVillagerEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+
+    public final AnimationState idleAnimationState = new AnimationState();
+    private int idleAnimationTimeout = 0;
+
+    public final AnimationState attackAnimationState = new AnimationState();
+    public int attackAnimationTimeout = 0;
+
+    private void setupAnimationStates() {
+        if (this.idleAnimationTimeout <= 0) {
+            this.idleAnimationTimeout = this.random.nextInt(40) + 80;
+            this.idleAnimationState.start(this.age);
+        } else {
+            this.idleAnimationTimeout--;
+        }
+
+        if(this.isAttacking() && attackAnimationTimeout <= 0) {
+            attackAnimationTimeout = 20;
+            attackAnimationState.start(this.age);
+        } else {
+            --this.attackAnimationTimeout;
+        }
+
+        if(!this.isAttacking()) {
+            attackAnimationState.stop();
+        }
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if(this.getWorld().isClient()){
+            setupAnimationStates();
+        }
+    }
+
+    public EvilVillagerEntity(EntityType<? extends HostileEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Override
+    public void initGoals(){
+        super.initGoals();
+        this.setPersistent();
+        this.goalSelector.add(1, new SwimGoal(this));
+        this.goalSelector.add(2, new EvilVillagerAttackGoal(this, 0.2, true));
+        this.goalSelector.add(3, new WanderAroundFarGoal(this, 0.8));
+        this.goalSelector.add(4, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
+        this.goalSelector.add(4, new LookAroundGoal(this));
+        this.targetSelector.add(1, new ActiveTargetGoal<>(this, PlayerEntity.class, true));
+    }
+
+    public static DefaultAttributeContainer.Builder createEvilVillagerAttributes() {
+        return MobEntity.createMobAttributes()
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 12)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.7f)
+                .add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 0.5)
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 0.5);
+    }
+
+    @Override
+    protected void updateLimbs(float posDelta) {
+        float f;
+        if (this.getPose() == EntityPose.STANDING) {
+            f = Math.min(posDelta * 6.0F, 1.0F);
+        } else {
+            f = 0.0F;
+        }
+
+        this.limbAnimator.updateLimbs(f, 0.2F);
+    }
+
+    public void setAttacking(boolean attacking) {
+        this.dataTracker.set(ATTACKING, attacking);
+    }
+
+    @Override
+    public boolean isAttacking() {
+        return this.dataTracker.get(ATTACKING);
+    }
+
+    @Override
+    protected void initDataTracker() {
+        super.initDataTracker();
+        this.dataTracker.startTracking(ATTACKING, false);
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource source) {
+        return SoundEvents.ENTITY_VILLAGER_HURT;
+    }
+
+    @Nullable
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return SoundEvents.ENTITY_VILLAGER_AMBIENT;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.ENTITY_VILLAGER_DEATH;
+    }
+
+    @Override
+    public boolean cannotDespawn() {
+        return true; // Forces the entity to never despawn
+    }
+
+    @Override
+    public boolean isDisallowedInPeaceful() {
+        return false; // Set to true if you want it to despawn in peaceful mode.
+    }
+    @Override
+    public boolean canImmediatelyDespawn(double distanceSquared) {
+        return false; // Prevents it from despawning naturally.
+    }
+
+    /*BOSS BAR*/
+
+    @Override
+    public void onStartedTrackingBy(ServerPlayerEntity player) {
+        super.onStartedTrackingBy(player);
+    }
+
+    @Override
+    public void onStoppedTrackingBy(ServerPlayerEntity player) {
+        super.onStoppedTrackingBy(player);
+    }
+
+    @Override
+    protected void mobTick() {
+        super.mobTick();
+    }
+}
